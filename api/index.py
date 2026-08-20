5abd5915120e2498a54228d90cc290284933115b
nirvijaya <vijayasekar.nirmal@gmail.com>
2026-08-20 14:50:59 +0300

    Test OpenAI environment variable

--- api/index.py
+++ api/index.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI  # type: ignore
-from fastapi.responses import PlainTextResponse  # type: ignore
-from openai import OpenAI  # type: ignore
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import os
 
 app = FastAPI()
 
 @app.get("/api", response_class=PlainTextResponse)
 def idea():
-    client = OpenAI()
-    prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
-    response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
-    return response.choices[0].message.content
+    key = os.environ.get("OPENAI_API_KEY")
+
+    if key:
+        return "OPENAI_API_KEY is available"
+    else:
+        return "OPENAI_API_KEY is NOT available"
